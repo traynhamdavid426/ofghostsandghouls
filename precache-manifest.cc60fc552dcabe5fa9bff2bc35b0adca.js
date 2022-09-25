f8b8c8064b5d6fd54c0965b7894a69f519cb2de2
traynhamdavid426 <113484217+traynhamdavid426@users.noreply.github.com>
2022-09-25 05:36:51 -0400

    Updates

---
+++ precache-manifest.cc60fc552dcabe5fa9bff2bc35b0adca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d0aef7f295b38374951",
+    "url": "/ofghostsandghouls/static/css/main.dfd63063.chunk.css"
+  },
+  {
+    "revision": "4d0aef7f295b38374951",
+    "url": "/ofghostsandghouls/static/js/main.6b9d756c.chunk.js"
+  },
+  {
+    "revision": "21b14cb7d5dac3e2a7af",
+    "url": "/ofghostsandghouls/static/js/runtime~main.9fcf6a42.js"
+  },
+  {
+    "revision": "170fc21c50ace3919478",
+    "url": "/ofghostsandghouls/static/css/2.763a8220.chunk.css"
+  },
+  {
+    "revision": "170fc21c50ace3919478",
+    "url": "/ofghostsandghouls/static/js/2.cf8dcc85.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/ofghostsandghouls/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "3b1846d78d37c00c26f4b2d09bd40e2e",
+    "url": "/ofghostsandghouls/index.html"
+  }
+];
